b2dd7f2d8486ac5fc54e253ca8eac61357cd6243
Valheri <valtteri.vuokila@gmail.com>
2025-03-22 14:26:58 +0200

    Tab menu tehtävä

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,38 @@
+import { Box, Tab, Tabs, Typography } from "@mui/material";
+import React, { useState } from "react";
 import "./App.css";
+import TodoList from "./TodoList";
 
-import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { Dayjs } from "dayjs";
-import { useState } from "react";
+type Todo = {
+  description: string;
+  priority: string;
+  date: string;
+};
 
 function App() {
-  const [selectedDate, setSelectedDate] = useState<Dayjs | null>(null);
+  const [activeTab, setActiveTab] = useState(0);
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  const handleDateChange = (newValue: Dayjs | null) => {
-    setSelectedDate(newValue);
+  const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+    setActiveTab(newValue);
   };
 
   return (
-    <>
-      <h1>Date</h1>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        
-        <DatePicker
-          label="Select a date"
-          value={selectedDate}
-          onChange={handleDateChange}
-          format="DD/MM/YYYY"
-        />
-      </LocalizationProvider>
-    </>
+    <Box  sx={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}>
+      <Tabs value={activeTab} onChange={handleTabChange} centered>
+        <Tab label="Home" />
+        <Tab label="Todo List" />
+      </Tabs>
+      <Box sx={{ padding: 2 }}>
+        {activeTab === 0 && (
+          <Typography variant="h4" component="div">
+            Welcome to the Home Page!
+          </Typography>
+        )}
+        {activeTab === 1 && <TodoList todos={todos} setTodos={setTodos} />}
+      </Box>
+    </Box>
   );
 }
+
 export default App;
