8f69de2a5fe1db18d364bec30f2087282a75cfd7
Valheri <valtteri.vuokila@gmail.com>
2025-04-05 15:59:01 +0300

    Autokauppa tehtävä

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Box, Tab, Tabs, Typography } from "@mui/material";
 import React, { useState } from "react";
 import "./App.css";
+import CarShop from "./CarShop"; // Import the new CarShop component
 import TodoList from "./TodoList";
 
 type Todo = {
@@ -18,18 +19,20 @@ function App() {
   };
 
   return (
-    <Box  sx={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}>
+    <Box sx={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}>
       <Tabs value={activeTab} onChange={handleTabChange} centered>
         <Tab label="Home" />
         <Tab label="Todo List" />
+        <Tab label="Car Shop" /> {/* New tab for Car Shop */}
       </Tabs>
       <Box sx={{ padding: 2 }}>
         {activeTab === 0 && (
           <Typography variant="h4" component="div">
-            Welcome to the Home Page!
+            Welcome to Home!
           </Typography>
         )}
         {activeTab === 1 && <TodoList todos={todos} setTodos={setTodos} />}
+        {activeTab === 2 && <CarShop />} {/* Render CarShop component */}
       </Box>
     </Box>
   );
