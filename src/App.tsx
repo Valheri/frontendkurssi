a57a94f5beae0bbfdac76359cb3d5875df1250b2
Valheri <valtteri.vuokila@gmail.com>
2025-04-18 19:13:43 +0300

    Lopputyö osa 3. Kalenterin lisäys

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import CalendarPage from "./CalendarPage";
 import CustomerList from "./CustomerList";
-import TrainingList from "./TrainingList";
 import "./styles.css";
+import TrainingList from "./TrainingList";
 
 function App() {
   return (
@@ -13,10 +14,14 @@ function App() {
         <Link to="/trainings" className="nav-link">
           Trainings
         </Link>
+        <Link to="/calendar" className="nav-link">
+          Calendar
+        </Link>
       </nav>
       <Routes>
         <Route path="/" element={<CustomerList />} />
         <Route path="/trainings" element={<TrainingList />} />
+        <Route path="/calendar" element={<CalendarPage />} />
       </Routes>
     </Router>
   );
---
+++ src/CalendarPage.tsx
@@ -0,0 +1,65 @@
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
+import { default as EventSourceInput, default as FullCalendar } from "@fullcalendar/react";
+
+import timeGridPlugin from "@fullcalendar/timegrid";
+import { useEffect, useState } from "react";
+
+const baseUrl = "https://customer-rest-service-frontend-personaltrainer.2.rahtiapp.fi/api/";
+
+const CalendarPage = () => {
+    const [events, setEvents] = useState<EventSourceInput>([]);
+
+    useEffect(() => {
+        fetchEvents();
+    }, []);
+
+    const fetchEvents = async () => {
+        try {
+            const res = await fetch(`${baseUrl}gettrainings`);
+            const data = await res.json();
+            const mappedEvents = data.map((training: any) => ({
+                id: training.id,
+                title: training.activity,
+                start: training.date,
+                extendedProps: {
+                    customer: training.customer ? `${training.customer.firstname} ${training.customer.lastname}` : "N/A"
+                }
+            }));
+            setEvents(mappedEvents);
+        } catch (error) {
+            console.error("Error fetching training events", error);
+        }
+    };
+
+    return (
+        <div className="calendar-page" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <h1>Trainings Calendar</h1>
+            <div style={{ width: "70%" }}>
+                <FullCalendar
+                    scrollTime="07:00:00"
+                    height="70vh"
+                    plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
+                    initialView="dayGridMonth"
+                    eventTimeFormat={{ hour: "2-digit", minute: "2-digit", hour12: false }}
+                    slotLabelFormat={{ hour: "2-digit", minute: "2-digit", hour12: false }}
+                    headerToolbar={{
+                        left: "prev,next today",
+                        center: "title",
+                        right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
+                    }}
+                    events={events}
+                    eventDidMount={(info) => {
+                        const customer = info.event.extendedProps.customer;
+                        if (customer && customer !== "N/A") {
+                            info.el.setAttribute("title", `Customer: ${customer}`);
+                        }
+                    }}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default CalendarPage;
